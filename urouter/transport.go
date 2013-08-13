ff1c224eb174caa5647860e0b468129284e7495e
Keith Rarick <kr@xph.us>
2013-08-12 17:51:53 -0700

    return complete http Response on Transport error

--- urouter/transport.go
+++ urouter/transport.go
@@ -3,6 +3,7 @@ package main
 import (
 	"io/ioutil"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
@@ -16,8 +17,22 @@ type Transport struct {
 func (t *Transport) RoundTrip(r *http.Request) (*http.Response, error) {
 	rt := t.Director(r)
 	if rt == nil {
-		body := ioutil.NopCloser(strings.NewReader(t.ErrBody))
-		return &http.Response{StatusCode: t.ErrCode, Body: body}, nil
+		n := len(t.ErrBody)
+		resp := &http.Response{
+			Body:       ioutil.NopCloser(strings.NewReader(t.ErrBody)),
+			Status:     strconv.Itoa(t.ErrCode) + " " + http.StatusText(t.ErrCode),
+			StatusCode: t.ErrCode,
+			Proto:      "HTTP/1.0",
+			ProtoMajor: 1,
+			ProtoMinor: 0,
+			Header: http.Header{
+				"Connection":     {"close"},
+				"Content-Length": {strconv.Itoa(n)},
+			},
+			Close:         true,
+			ContentLength: int64(n),
+		}
+		return resp, nil
 	}
 	return rt.RoundTrip(r)
 }
--- urouter/transport_test.go
+++ urouter/transport_test.go
@@ -3,33 +3,62 @@ package main
 import (
 	"io/ioutil"
 	"net/http"
+	"reflect"
 	"testing"
 )
 
 func TestTransportErr(t *testing.T) {
 	tr := &Transport{
-		Director: func(*http.Request) http.RoundTripper {
-			return nil
-		},
-		ErrCode: 456,
-		ErrBody: "foo",
+		Director: func(*http.Request) http.RoundTripper { return nil },
+		ErrCode:  500,
+		ErrBody:  "foo",
 	}
 	req, err := http.NewRequest("GET", "http://example.com/", nil)
 	if err != nil {
 		t.Fatal("unexpected error", err)
 	}
-	resp, err := tr.RoundTrip(req)
+	g, err := tr.RoundTrip(req)
 	if err != nil {
 		t.Fatal("unexpected error", err)
 	}
-	if resp.StatusCode != tr.ErrCode {
-		t.Errorf("StatusCode = %d want %d", resp.StatusCode, tr.ErrCode)
-	}
-	body, err := ioutil.ReadAll(resp.Body)
+
+	const wbody = "foo"
+	gbody, err := ioutil.ReadAll(g.Body)
 	if err != nil {
 		t.Fatal("unexpected error", err)
 	}
-	if g := string(body); g != tr.ErrBody {
-		t.Fatalf("body = %q want %q", g, tr.ErrBody)
+	if string(gbody) != wbody {
+		t.Fatalf("body = %q want %q", string(gbody), wbody)
+	}
+
+	w := &http.Response{
+		Status:     "500 Internal Server Error",
+		StatusCode: 500,
+		Proto:      "HTTP/1.0",
+		ProtoMajor: 1,
+		ProtoMinor: 0,
+		Header: http.Header{
+			"Connection":     {"close"},
+			"Content-Length": {"3"},
+		},
+		Close:         true,
+		ContentLength: 3,
+	}
+	g.Body = nil
+	diff(t, "Response", g, w)
+}
+
+func diff(t *testing.T, prefix string, have, want interface{}) {
+	hv := reflect.ValueOf(have).Elem()
+	wv := reflect.ValueOf(want).Elem()
+	if hv.Type() != wv.Type() {
+		t.Errorf("%s: type mismatch %v want %v", prefix, hv.Type(), wv.Type())
+	}
+	for i := 0; i < hv.NumField(); i++ {
+		hf := hv.Field(i).Interface()
+		wf := wv.Field(i).Interface()
+		if !reflect.DeepEqual(hf, wf) {
+			t.Errorf("%s: %s = %v want %v", prefix, hv.Type().Field(i).Name, hf, wf)
+		}
 	}
 }
