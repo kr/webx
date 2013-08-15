31cc4fce78fd82624c2e3a7b07d0a1848df68792
Keith Rarick <kr@xph.us>
2013-08-14 17:32:53 -0700

    script for easy testing and dev

---
+++ try.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+go get github.com/kr/fernet/fernet-sign
+go get github.com/kr/webx/...
+
+export FERNET_KEY=`dd if=/dev/urandom bs=1 count=32 2>/dev/null|openssl base64`
+export TOK=`printf foo|fernet-sign FERNET_KEY`
+export REQADDR=:8000
+export REQTLSADDR=:4443
+export BKDADDR=:1111
+export PORT=5000
+export WEBX_URL="https://foo:$TOK@127.0.0.1:1111/"
+export WEBX_VERBOSE=1
+
+urouter &
+webxd &
+"$@" &
+
+trap 'kill -9 %1 %2 %3' EXIT
+wait
